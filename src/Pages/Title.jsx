0bbcaeeb7dd185592bfc68a18748638581a7e237
go2 <rio214@naver.com>
2024-04-12 14:36:06 +0900

    v.1(0412.2)

---
+++ src/Pages/Title.jsx
@@ -0,0 +1,43 @@
+import styles from '../css/Title.module.css';
+import React, {useState} from 'react';
+function Title() {
+    const [inputValue, setInputValue] = useState('');
+    const maxLength = 12;
+
+    const handleChange = (event) => {
+      const {value} = event.target;
+      if (value.length <= maxLength){
+        setInputValue(value);
+      }
+    };
+    const [textValue,setTextValue] = useState('');
+    const textLength = 50;
+    const textCh = (event) => {
+      const {value} = event.target;
+      if (value.length <= textLength){
+        setTextValue(value);
+      }
+    };
+
+    return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+          <h1>제목</h1>
+        <div className={styles.inputStyle}>
+        <input type="text" 
+        placeholder="제목을 입력해주세요"
+        value={inputValue} 
+        onChange={handleChange}
+        />
+        {inputValue.length}/{maxLength}
+        </div>
+        <div className={styles.textbox}>
+          <input type="text" placeholder='내용을 입력해주세요' value={textValue} onChange={textCh}/>
+          {textValue.length}/{textLength}
+        </div>
+      </div>
+    </div>  
+    );
+  }
+  
+export default Title;
